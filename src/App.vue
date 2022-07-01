a1dee9769f2c0411c9f5e9ba0d027fe4e9ba4b1b
WEB WEST <web.west5@gmail.com>
2022-07-01 17:30:42 +0300

    [+] Create modal with teleport and composition api

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <ModalComponent id="1" header="Header-1" content="Content-1" />
+  <ModalComponent id="2" header="Header-2" content="Content-2" />
+  <ModalComponent id="3" header="Header-3" content="Content-3" />
+  <!-- <ModalComponent header="Header-2" content="Content-2" /> -->
+</template>
+
+<script>
+import ModalComponent from "./components/ModalComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    ModalComponent,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <button class="modal-open" @click="isOpen = true">Open Modal {{id}}</button>
+  <teleport to="body">
+    <transition name="opacity">
+      <div class="modal" v-if="isOpen">
+        <div class="modal-wrapper">
+          <div class="modal-header">{{ header }}</div>
+          <div class="modal-content">{{ content }}</div>
+          <div class="modal-buttons">
+            <button @click="isOpen = false">Close</button>
+          </div>
+        </div>
+      </div>
+    </transition>
+  </teleport>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  props: {
+    id: {
+      type: [Number,String],
+      default: "",
+    },
+    header: {
+      type: String,
+      default: "",
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+  },
+  setup(props) {
+    const isOpen = ref(false);
+    return { isOpen, props };
+  },
+};
+</script>
